430cd2cd3cdd43b61a47c3a32c7c2c0f02888b8b
javier <javieriranzo3@gmail.com>
2020-09-27 18:33:24 +0200

    commit segundo

--- cypress/integration/geb_failure_spec.js
+++ cypress/integration/geb_failure_spec.js
@@ -4,18 +4,7 @@ cypress run --spec cypress\integration\geb-failure_spec.js
 describe('GEB failure', () => {
     
     it('Fail loads GEB.com', () => {
-        cy.visit('https://preaplicaciones.aragon.es/geb/')
+        cy.visit('http://automationpractice.com/index.php')
     })
 
-    it('Login GEB', () => {
-        cy.get('#processLogin_idUsuario')
-        .type('jiranzo')
-        cy.get('#processLogin_password')
-        .type('test')
-        cy.get('[type="submit"]')
-        .click()
-        cy.contains('JAVIER BURRIEL IRANZO')
-        cy.get(':nth-child(10) > .seemTable2 > :nth-child(5) > .textoApl1 > a')
-        .click()
-    })
   })
